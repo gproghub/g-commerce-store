6b47ccc6930915de3365dc9f6b212a13eca20925
gdevcodes <gprog@tutanota.com>
2023-08-09 18:33:03 +0200

    commit-final-2

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -7,17 +7,35 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ className, children, disabled, type = 'button', ...props }, ref) => {
   return (
     <button
-      ref={ref}
-      {...props}
+      type={type}
       className={cn(
-        'w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opcacity-75 transition',
+        `
+        w-auto 
+        rounded-full 
+        bg-black
+        border
+        border-transparent
+        px-5 
+        py-3 
+        disabled:cursor-not-allowed 
+        disabled:opacity-50
+        text-white
+        font-semibold
+        hover:opacity-75
+        transition
+      `,
+        disabled && 'opacity-75 cursor-not-allowed',
         className
       )}
       disabled={disabled}
+      ref={ref}
+      {...props}
     >
       {children}
     </button>
   );
 });
+
+Button.displayName = 'Button';
 
 export default Button;
